6c00641aa94f0d7cba44a43ff47d92de333ff157
Axl Yody <axlyody@gmail.com>
2016-01-24 01:49:35 +0000

    ...

--- assets/js/spaghetti.js
+++ assets/js/spaghetti.js
@@ -1,1 +1,58 @@
-Pace.on("start",function(){$(".pacel").show()}),Pace.on("done",function(){$(".pacel").hide()});var options={url:"/gblk.json",getValue:"title",list:{match:{enabled:!0}},template:{type:"links",fields:{link:"url"}},theme:"square"};$("#countries").easyAutocomplete(options),$(function(){$(document).pjax("a","body",{fragment:"body",timeout:1e5})}),$(document).on("pjax:error",function(e,t,n,o,a){return a.success(t.responseText,n,t),!1});var waterfall=new Waterfall({containerSelector:".wf-container",boxSelector:".wf-box",minBoxWidth:180});$(function(){$("img").lazyload({effect:"fadeIn"})});
+
+   
+
+var options = {
+
+  url: "/gblk.json",
+
+  getValue: "title",
+
+
+   list: {
+		match: {
+			enabled: true
+		}
+	},
+  
+    template: {
+        type: "links",
+        fields: {
+            link: "url"
+        }
+        
+    },
+  
+
+  theme: "square"
+};
+
+$("#countries").easyAutocomplete(options);
+
+
+
+   Pace.on("start", function(){
+    $(".pacel").show();
+   });
+   Pace.on("done", function(){
+    $(".pacel").hide();
+   });
+   
+ 
+$(function() {
+    $("img").lazyload({
+      effect : "fadeIn",
+      });
+});
+
+  // use querySelector/querySelectorAll internally
+var waterfall = new Waterfall({ 
+    containerSelector: '.wf-container',
+    boxSelector: '.wf-box',
+    minBoxWidth: 180
+});
+
+  $(document).ready(function(){
+    $(".menu-icon").click(function(){
+        $(".trigger").toggle();
+    });
+});
